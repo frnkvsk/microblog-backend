b2e86be78feb4afa9d9c2f51c7aaf24d7eb7b169
Frank Vasek <frnk05vas@zoho.com>
2020-10-25 07:22:09 -0700

    update db.js to work on heroku

--- db.js
+++ db.js
@@ -10,11 +10,3 @@ const client = new Client({
 });
 
 client.connect();
-
-client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-  if (err) throw err;
-  for (let row of res.rows) {
-    console.log(JSON.stringify(row));
-  }
-  client.end();
-});
